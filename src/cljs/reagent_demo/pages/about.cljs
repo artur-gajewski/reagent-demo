0457ccab8d34d5939d374c084ad38dabed08787f
Artur Gajewski (fraktio) <artur.gajewski@fraktio.fi>
2016-09-09 08:51:59 +0300

    Added info about me and few links

--- src/cljs/reagent_demo/pages/about.cljs
+++ src/cljs/reagent_demo/pages/about.cljs
@@ -16,7 +16,13 @@
    (when @state/message-revealed
      [:div
       [:h3 "Reagent is React with ClojureScript"]
+
       [:p
        "Reagent provides a minimalistic interface between ClojureScript and React. It allows you to define efficient React components using nothing but plain ClojureScript functions and data, that describe your UI using a Hiccup-like syntax."]
+
       [:p
-       "The goal of Reagent is to make it possible to define arbitrarily complex UIs using just a couple of basic concepts, and to be fast enough by default that you rarely have to care about performance."]])])
+       "The goal of Reagent is to make it possible to define arbitrarily complex UIs using just a couple of basic concepts, and to be fast enough by default that you rarely have to care about performance."]
+
+      [:p
+       "More about Reagent: "
+       [:a {:href "https://reagent-project.github.io/"} "Reagent Project @ Github"]]])])
--- src/cljs/reagent_demo/pages/home.cljs
+++ src/cljs/reagent_demo/pages/home.cljs
@@ -6,6 +6,11 @@
    [title/render "Welcome to Reagent demo"]
 
    [:div
+    [:p "This site is built on Reagent, a ClojureScript wrapper for React which is transpiled to native javascript
+     React application. All pages on this site feature functions coded with Reagent. Feel free to click around and
+     see how Reagent performs, and don't forget to check the Github repository for source code of this boilerplate."]]
+
+   [:div
     [:a {:href "/about"} "» Read about Reagent"]]
 
    [:div
@@ -18,4 +23,17 @@
     [:a {:href "/item/3"} "» Route parameter"]]
 
    [:div
-    [:a {:href "https://github.com/artur-gajewski/reagent-demo"} "» Source code"]]])
+    [:a {:href "https://github.com/artur-gajewski/reagent-demo"} "» Source code"]]
+
+   [:div
+    [:h3 "About me"]
+
+    [:p "Twitter: "
+     [:a
+      {:href "https://twitter.com/GajewskiArtur"}
+      "https://twitter.com/GajewskiArtur"]]
+
+    [:p "LinkedIn: "
+     [:a
+      {:href "https://fi.linkedin.com/in/arturgajewski"}
+      "https://fi.linkedin.com/in/arturgajewski"]]]])
