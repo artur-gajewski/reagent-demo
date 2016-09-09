d54bc2ad68e946244962f6a3ca599173475b57cc
Artur Gajewski (fraktio) <artur.gajewski@fraktio.fi>
2016-09-09 09:17:54 +0300

    Added instrution to chnage the ID

--- src/cljs/reagent_demo/pages/item.cljs
+++ src/cljs/reagent_demo/pages/item.cljs
@@ -9,6 +9,10 @@
    [:div
     [:a {:href "/"} "« Back to the main page"]]
 
-     [:div
-      [:p
-       (str "You requested page with item ID: " @state/item-id)]]])
+    [:div
+     [:p
+      (str "You requested page with item ID: " @state/item-id)]]
+
+    [:div
+     [:p
+      "Go ahead and change the ID number in the URL ;-)"]]])
